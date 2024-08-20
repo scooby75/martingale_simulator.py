14618f015579109223782208cc4557c6a8c1062b
scooby75 <45230721+scooby75@users.noreply.github.com>
2024-08-20 10:46:37 -0300

    Update martingale_simulator.py

--- martingale_simulator.py
+++ martingale_simulator.py
@@ -1,17 +1,14 @@
 import streamlit as st
 
-def calcular_martingale(qtd_perdas, odd_back, comissao, valor_inicial):
+def calcular_martingale_normal(odd_back, valor_inicial, qtd_apostas):
     apostas = []
-    total_perda = 0
     valor_aposta = valor_inicial
+    total_perda = 0
 
-    for i in range(qtd_perdas):
-        # Calcula o valor da aposta
+    for i in range(qtd_apostas):
         aposta = valor_aposta
         total_perda += aposta
-        
-        # Calcula o lucro previsto (ajustado pela comissão)
-        lucro_previsto = aposta * (odd_back - 1) * (1 - comissao / 100)
+        lucro_previsto = aposta * (odd_back - 1)
 
         apostas.append({
             'Aposta': i + 1,
@@ -20,7 +17,7 @@ def calcular_martingale(qtd_perdas, odd_back, comissao, valor_inicial):
             'Lucro Previsto': round(lucro_previsto, 2)
         })
         
-        # Calcula o próximo valor da aposta para Martingale
+        # Calcula o valor da próxima aposta para cobrir todas as perdas
         valor_aposta = (total_perda + valor_inicial) / (odd_back - 1)
 
     return apostas
@@ -28,13 +25,12 @@ def calcular_martingale(qtd_perdas, odd_back, comissao, valor_inicial):
 def main():
     st.title("Simulador de Recuperação de Perdas - Martingale")
 
-    qtd_perdas = st.number_input("Quantidade de apostas", min_value=1, value=3)
     odd_back = st.number_input("Odd Back", min_value=1.01, value=2.0)
-    comissao = st.number_input("Comissão Exchange (%)", min_value=0.0, max_value=100.0, value=5.0)
     valor_inicial = st.number_input("Valor Inicial da Aposta", min_value=1.0, value=1.0)
+    qtd_apostas = st.number_input("Quantidade de Apostas", min_value=1, value=3)
 
     if st.button("Calcular"):
-        apostas = calcular_martingale(qtd_perdas, odd_back, comissao, valor_inicial)
+        apostas = calcular_martingale_normal(odd_back, valor_inicial, qtd_apostas)
         
         st.write("### Detalhamento das Apostas")
         st.write("Aposta | Valor | Total de Perda | Lucro Previsto")
