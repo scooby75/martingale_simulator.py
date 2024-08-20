de5b6d04939879c40b7eada7a4e4ea51b81b1186
scooby75 <45230721+scooby75@users.noreply.github.com>
2024-08-20 11:20:54 -0300

    Update martingale_simulator.py

--- martingale_simulator.py
+++ martingale_simulator.py
@@ -4,12 +4,15 @@ import pandas as pd
 def calcular_martingale(banca_inicial, valor_aposta, odd_back, qnt_vezes, comissao):
     dados = []
     valor_atual_aposta = valor_aposta
+    perda_total = 0
     banca_restante = banca_inicial
     
     for i in range(1, qnt_vezes + 1):
+        if i > 1:
+            valor_atual_aposta = perda_total / (odd_back - 1) + valor_aposta
+        
         aposta_total = valor_atual_aposta
-        perda = aposta_total if i > 1 else 0
-        valor_atual_aposta *= 2
+        perda_total += aposta_total
         
         lucro_bruto = (odd_back - 1) * valor_aposta
         lucro_liquido = lucro_bruto * (1 - comissao / 100)
@@ -17,13 +20,13 @@ def calcular_martingale(banca_inicial, valor_aposta, odd_back, qnt_vezes, comiss
         dados.append({
             "Rodada": i,
             "Valor Apostado (R$)": aposta_total,
-            "Perda (R$)": perda,
+            "Perda Acumulada (R$)": perda_total,
             "Valor da Próxima Entrada (R$)": valor_atual_aposta,
             "Lucro Líquido (R$)": lucro_liquido
         })
         
         banca_restante -= aposta_total
-        
+    
     # Calcula o lucro total obtido
     lucro_total = lucro_liquido - (valor_aposta * (2**qnt_vezes - 1))
     
@@ -46,9 +49,10 @@ if st.button("Calcular"):
     st.subheader("Tabela de Apostas")
     st.write(df)
     
-    #st.subheader("Resultados Finais")
-    #st.write(f"Lucro Total Obtido (R$): {lucro_total:.2f}")
+    st.subheader("Resultados Finais")
+    st.write(f"Lucro Total Obtido (R$): {lucro_total:.2f}")
     if suficiente_para_banca:
         st.success("A banca inicial é suficiente para cobrir as apostas.")
     else:
         st.error("A banca inicial não é suficiente para cobrir as apostas.")
+
